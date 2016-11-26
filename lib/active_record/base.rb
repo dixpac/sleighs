2814a8cafa5cb5e0fbd6c9750a147b8bd683211c
dixpac <dino.onex@gmail.com>
2016-11-26 20:09:56 +0100

    Add where, order methods
    Methods are using ActiveRecord::Relation class which is chainable.
    Also where! and order! methods are added, which don't make new Relation
    class for each new record.

--- lib/active_record/base.rb
+++ lib/active_record/base.rb
@@ -27,7 +27,15 @@ module ActiveRecord
     end
 
     def self.all
-      find_by_sql("SELECT * FROM #{table_name}")
+      Relation.new(self)
+    end
+
+    def self.where(*args)
+      all.where(*args)
+    end
+
+    def self.order(*args)
+      all.order(*args)
     end
 
     def self.find_by_sql(sql)
