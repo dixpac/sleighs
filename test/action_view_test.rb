ab0067a6c666793b7af6af96935cee49c7d420e6
dixpac <dino.onex@gmail.com>
2016-12-16 17:11:45 +0100

    Bind variable and view

--- test/action_view_test.rb
+++ test/action_view_test.rb
@@ -7,4 +7,11 @@ class ActionViewTest < Minitest::Test
 
     assert_equal "<p>Hi!</p>", template.render(context)
   end
+
+  def test_render_variables
+    template = ActionView::Template.new("<p><%= @msg %></p>", 'test_render_template')
+    context = ActionView::Base.new(msg: 'Hi!')
+
+    assert_equal "<p>Hi!</p>", template.render(context)
+  end
 end
